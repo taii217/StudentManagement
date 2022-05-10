708d131323d1eb2985b60e1245c9a06ffd949cb8
taii217 <taiinguyen217@gmail.com>
2022-05-10 11:25:14 +0700

    tai UI 2.0

--- studentManagement/management/views.py
+++ studentManagement/management/views.py
@@ -3,5 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,'management/main.html')
+    return render(request,'main.html')
+
+def login(request):
+    return render(request,'login.html')
+ 
+def logout(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')
  
