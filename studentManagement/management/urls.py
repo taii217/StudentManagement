e371660c7ab992ba80194216fc4d764c80b3766f
taii217 <taiinguyen217@gmail.com>
2022-05-22 21:38:40 +0700

    local test

--- studentManagement/management/urls.py
+++ studentManagement/management/urls.py
@@ -7,8 +7,10 @@ urlpatterns = [
     path('login/',views.login,name="login"),
     path('logout/',views.logout,name="logout"),
     path('register/',views.register,name="register"),
+
     path('rules/',views.rules,name="rules"),
     path('insert_grade/',views.insert_grade,name="insert_grade"),
+
     path('subject_summary/',views.subject_summary,name="subject_summary"),
     path('final_summary/',views.final_summary,name="final_summary")
 ]
