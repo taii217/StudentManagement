a79c58d60e80d21116bbc8d3fde266c802688a2f
Khanh Toan <laikhanhtoanb10@gmail.com>
2022-06-01 23:32:37 +0700

    update previous task for fixing some bugs

--- studentManagement/management/filters.py
+++ studentManagement/management/filters.py
@@ -5,3 +5,4 @@ class ClassFilter(django_filters.FilterSet):
     class Meta:
         model = Class
         fields = ['ID', 'school_year']
+        
--- studentManagement/studentManagement/settings.py
+++ studentManagement/studentManagement/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'management',
     'django_filters',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
